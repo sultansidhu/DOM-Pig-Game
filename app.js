0e31bc7aaf6789ba133d151e5426492c30785dbd
sultansidhu <sultansinghsidhu@gmail.com>
2018-11-05 00:42:34 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,143 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying, previousRoll, rollCounter, randomArray, limit;
+
+function init(){
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+    previousRoll = 0;
+    rollCounter = 0;
+    randomArray = [];
+    limit = 100;
+    
+    document.querySelector('.dice').style.display = "none";
+    
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.add('active'); 
+}
+
+init();
+
+function setTopValue(){
+    var value = document.getElementById('limit').value;
+    console.log(value);
+    if (value > 1){
+        return value;
+    } else {
+        return 100
+    }
+}
+
+function nextPlayer(){
+    activePlayer === 0? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    
+    // makes the current score to 0 with every turn switch
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    // switches the currently active player
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+    // makes the dice disappear before the user starts rolling
+    document.querySelector('.dice').style.display = 'none';
+}
+
+function setPreviousRoll(){
+    if (rollCounter === 0){
+        previousRoll = 0;
+    } else {
+        previousRoll = randomArray[rollCounter - 1];
+        // previousRoll = dice;
+        
+    }
+}
+
+// adding listener for the roll button
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if (gamePlaying){
+        // generate a random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+        randomArray.push(dice);
+        //console.log(randomArray);
+        setPreviousRoll();
+       // console.log('current roll is ' + dice);
+        //console.log('previous roll was ' + previousRoll);
+        
+        
+        // displaying the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = "block";
+        diceDOM.src = "dice-" + dice + ".png";
+        
+        // incrementing the rollCounter
+        rollCounter ++;
+        
+        //score updation unless number rolled is 1
+        if (dice!==1){
+            roundScore += dice;
+            document.getElementById('current-'+activePlayer).textContent = roundScore;
+        } else if (dice === 6 && previousRoll === 6){
+            document.getElementById('current-'+ activePlayer).textContent = '0';
+            document.getElementById('score-'+ activePlayer).textContent = '0';
+            nextPlayer();
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+
+// adding listener for the hold button
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // update scores
+    scores[activePlayer] += roundScore;
+    
+    // update UI
+    document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+    
+    // check for winners
+    if (scores[activePlayer] >= limit){
+        document.getElementById('name-' + activePlayer).textContent = "Winner!";
+        document.querySelector('.dice').style.display = 'none';
+        
+        // add winner status to the player panel
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        gamePlaying = false;
+    } else {
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-getnum').addEventListener('click', function(){
+    limit = setTopValue();
+});
+
+// adding listener for a new game
+document.querySelector('.btn-new').addEventListener('click', function(){
+    init();
+});
+
+
